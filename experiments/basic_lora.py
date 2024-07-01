7f89e8c43b7ead6985cecc1321ef78966d50d6a9
sambowyer <sambowyer2@gmail.com>
2024-07-01 13:44:15 +0100

    basic examples

---
+++ experiments/basic_lora.py
@@ -0,0 +1,55 @@
+# import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer, TrainingArguments, Trainer, DataCollatorForLanguageModeling
+from datasets import load_dataset
+from peft import LoraConfig, get_peft_model
+
+# Load dataset
+dataset = load_dataset('wikitext', 'wikitext-2-raw-v1')
+                       
+# Load pre-trained model and tokenizer
+model_name = 'gpt2'
+model = AutoModelForCausalLM.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+tokenizer.pad_token = tokenizer.eos_token
+
+# Integrate LoRA adapters
+lora_config = LoraConfig(r=4)
+model = get_peft_model(model, lora_config)
+
+# Tokenize dataset
+def tokenize_function(examples):
+    return tokenizer(examples['text'], padding="max_length", truncation=True)
+
+tokenized_datasets = dataset.map(tokenize_function, batched=True)
+
+# Data collator
+data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
+
+# Training arguments
+training_args = TrainingArguments(
+    output_dir="./results",
+    evaluation_strategy="epoch",
+    learning_rate=2e-5,
+    weight_decay=0.01,
+    per_device_train_batch_size=8,
+    per_device_eval_batch_size=8,
+    num_train_epochs=3,
+    save_steps=10_000,
+    save_total_limit=2,
+)
+
+# Initialize Trainer
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=tokenized_datasets['train'],
+    eval_dataset=tokenized_datasets['test'],
+    data_collator=data_collator,
+)
+
+# Fine-tune the model
+trainer.train()
+
+# Save the fine-tuned model
+model.save_pretrained("./fine-tuned-model")
+tokenizer.save_pretrained("./fine-tuned-model")
---
+++ experiments/hf.py
@@ -0,0 +1,16 @@
+# %%
+from transformers import pipeline
+
+generator = pipeline('text-generation', model='distilgpt2')
+
+# %%
+text = generator("In this essay, I will discuss", max_length=100, num_return_sequences=5)
+
+# # %%
+# print(text)
+# breakpoint()
+
+# %%
+unmasker = pipeline('fill-mask')
+text = unmasker("This is <mask>", top_k=5)
+print(text)
